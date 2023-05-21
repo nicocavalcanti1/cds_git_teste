9f381ed4366d0072055b9343186bce9bbbbb5119
Nicolas Cavalcanti <nicocavalcanti@gmail.com>
2023-05-21 11:48:46 -0300

    Adicionaado operação

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def gether_data():
-    n1 = int(input("Primeiro valor: "))
-    n2 = int(input("Segundo valor: "))
+    n1 = input("Primeiro valor: ")
+    n2 = input("Segundo valor: ")
+    op = input("Operação: ")
 
-    return n1, n2
+    return n1, n2, op
 
 def main():
-    n1, n2 = gether_data()
+    n1, n2, op = gether_data()
     
-    print(n1**n2)
+    print((eval(n1+op+n2)))
     
     return None
 
